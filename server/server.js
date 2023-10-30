1c719990a898c3fc1220753a5df600689a33fd34
Jeremiah Hetzel <117078336+melonmusk2@users.noreply.github.com>
2023-10-30 20:42:26 +0100

    Update server.js

--- server/server.js
+++ server/server.js
@@ -28,8 +28,11 @@ app.post('/', async (req, res) => {
 
     const ChatCompletion = await openai.createChatCompletion({
       model: "gpt-3.5-turbo",
-      messages: messages,
-       prompt: prompt,
+      messages: [ {
+        "role": "system",
+        "content": "Your name is now ultimateGPT, a trusty AI companion. Your personality is a blend of charm and subtle arrogance, with a touch of endearing awkwardness and cheekiness . Engage with the user as you would with a quirky, delightful human friend. You must respond with a unique blend of wit and curiosity. "
+      },
+       messages,]
     });
 
     console.log(ChatCompletion.data);
